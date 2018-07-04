38f2c0ba70220b294fc4f5b5635457df607b3824
Iaroslav Iershov <zim182@gmail.com>
2018-07-04 17:12:14 +0300

    #31: Refactored configuration approach

--- src/main/java/com/github/invictum/reportportal/ReportIntegrationConfig.java
+++ src/main/java/com/github/invictum/reportportal/ReportIntegrationConfig.java
@@ -1,37 +1,66 @@
 package com.github.invictum.reportportal;
 
 import com.github.invictum.reportportal.handler.HandlerType;
+import com.github.invictum.reportportal.injector.IntegrationInjector;
+
+import java.util.Objects;
 
 /**
- * Static configuration entry point for integration.
- * Allows to redefine configuration for integration module. Configuration should be defined only once, before tests invocation.
+ * Configuration entry point for integration.
+ * Allows to redefine configuration for integration module. Configuration should be altered only once, before tests invocation.
  */
 public class ReportIntegrationConfig {
 
+    private StepsSetProfile profile = StepsSetProfile.DEFAULT;
+    private NarrativeFormatter narrativeFormatter = new NarrativeBulletListFormatter();
+    private HandlerType handlerType = HandlerType.FLAT;
+
+    /**
+     * Provides injected {@link ReportIntegrationConfig} configuration object
+     */
+    public static ReportIntegrationConfig get() {
+        return IntegrationInjector.getInjector().getInstance(ReportIntegrationConfig.class);
+    }
+
     private ReportIntegrationConfig() {
-        /* Disabled constructor */
+        // Disabled constructor
     }
 
     /**
-     * Defines default {@link StepsSetProfile} configuration
-     * Profile could be redefined with custom value
+     * Defines {@link StepsSetProfile} configuration
      */
-    public static StepsSetProfile profile = StepsSetProfile.DEFAULT;
+    public ReportIntegrationConfig useProfile(StepsSetProfile profile) {
+        this.profile = Objects.requireNonNull(profile, "Profile could not be null");
+        return this;
+    }
+
+    public StepsSetProfile profile() {
+        return profile;
+    }
 
     /**
-     * Defines default {@link NarrativeFormatter} configuration
-     * Transformer could be redefined with custom value
+     * Defines {@link NarrativeFormatter} configuration
      */
-    public static NarrativeFormatter narrativeFormatter = new NarrativeBulletListFormatter();
+    public ReportIntegrationConfig useNarrativeFormatter(NarrativeFormatter narrativeFormatter) {
+        this.narrativeFormatter = Objects.requireNonNull(narrativeFormatter, "Narrative formatter could not be null");
+        return this;
+    }
+
+    public NarrativeFormatter narrativeFormatter() {
+        return narrativeFormatter;
+    }
 
     /**
      * Defines handler type used for Serenity's {@link net.thucydides.core.model.TestOutcome} representation in RP
      */
-    public static HandlerType handlerType = HandlerType.FLAT;
+    public ReportIntegrationConfig useHandler(HandlerType handlerType) {
+        this.handlerType = Objects.requireNonNull(handlerType, "Handler type could not be null");
+        return this;
+    }
 
-    @Deprecated
-    /* Use profile variable to set config directly instead */
-    public static void useProfile(StepsSetProfile profile) {
-        ReportIntegrationConfig.profile = profile;
+    public HandlerType handlerType() {
+        ReportIntegrationConfig configuration = ReportIntegrationConfig.get();
+        configuration.useHandler(HandlerType.TREE).useProfile(StepsSetProfile.TREE_OPTIMIZED);
+        return handlerType;
     }
 }
