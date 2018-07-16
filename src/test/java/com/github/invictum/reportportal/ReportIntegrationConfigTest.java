b710faf5c133ac25d73c8a249168d3ef2fe3e0fd
Iaroslav Iershov <zim182@gmail.com>
2018-07-16 18:53:28 +0300

    #38: Change narrative formatter approach  - turn formatter into function  - updated unit tests  - removed unused classes  - updated documentation

--- src/test/java/com/github/invictum/reportportal/ReportIntegrationConfigTest.java
+++ src/test/java/com/github/invictum/reportportal/ReportIntegrationConfigTest.java
@@ -1,11 +1,13 @@
 package com.github.invictum.reportportal;
 
 import com.github.invictum.reportportal.handler.HandlerType;
+import net.thucydides.core.annotations.Narrative;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
+import org.mockito.Mockito;
 
 @RunWith(JUnit4.class)
 public class ReportIntegrationConfigTest {
@@ -40,8 +42,12 @@ public class ReportIntegrationConfigTest {
 
     @Test
     public void defaultNarrativeFormatterTest() {
-        Class actual = config.narrativeFormatter().getClass();
-        Assert.assertEquals(NarrativeBulletListFormatter.class, actual);
+        Narrative narrativeMock = Mockito.mock(Narrative.class);
+        Mockito.when(narrativeMock.title()).thenReturn("Title");
+        Mockito.when(narrativeMock.text()).thenReturn(new String[]{"line 1", "line 2"});
+        config.resetToDefaults();
+        String actual = config.narrativeFormatter().apply(narrativeMock);
+        Assert.assertEquals("**Title**\n* line 1\n* line 2", actual);
     }
 
     @Test(expected = NullPointerException.class)
