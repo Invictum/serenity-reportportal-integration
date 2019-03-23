d1edc1dfa1f19cf4bcb2d5638947ec13ff6962e8
Iaroslav Iershov <zim182@gmail.com>
2019-03-23 15:13:13 -0500

    #60: Added unit test for start event builder

---
+++ src/test/java/com/github/invictum/reportportal/StartEventBuilderTest.java
@@ -0,0 +1,97 @@
+package com.github.invictum.reportportal;
+
+import com.epam.ta.reportportal.ws.model.ParameterResource;
+import com.epam.ta.reportportal.ws.model.StartTestItemRQ;
+import net.thucydides.core.model.DataTable;
+import net.thucydides.core.model.TestTag;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+import org.mockito.Mockito;
+
+import java.time.ZonedDateTime;
+import java.util.Collections;
+import java.util.Date;
+import java.util.HashSet;
+import java.util.Set;
+
+@RunWith(JUnit4.class)
+public class StartEventBuilderTest {
+
+    @Test
+    public void itemTypeTest() {
+        StartTestItemRQ event = new StartEventBuilder(ItemType.TEST)
+                .withStartTime(ZonedDateTime.now())
+                .withName("name")
+                .build();
+        Assert.assertEquals("STEP", event.getType());
+    }
+
+    @Test
+    public void withStartTimeTest() {
+        ZonedDateTime time = ZonedDateTime.now();
+        StartTestItemRQ event = new StartEventBuilder(ItemType.TEST)
+                .withStartTime(time)
+                .withName("name")
+                .build();
+        Assert.assertEquals(Date.from(time.toInstant()), event.getStartTime());
+    }
+
+    @Test(expected = NullPointerException.class)
+    public void withNullStartTimeTest() {
+        new StartEventBuilder(ItemType.TEST).withStartTime(null).withName("name").build();
+    }
+
+    @Test
+    public void withNameTest() {
+        StartTestItemRQ event = new StartEventBuilder(ItemType.TEST)
+                .withStartTime(ZonedDateTime.now())
+                .withName("name")
+                .build();
+        Assert.assertEquals("name", event.getName());
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void withNullNameTest() {
+        new StartEventBuilder(ItemType.TEST).withStartTime(ZonedDateTime.now()).withName(null).build();
+    }
+
+    @Test
+    public void withDescriptionTest() {
+        StartTestItemRQ event = new StartEventBuilder(ItemType.TEST)
+                .withStartTime(ZonedDateTime.now())
+                .withName("name")
+                .withDescription("description")
+                .build();
+        Assert.assertEquals("description", event.getDescription());
+    }
+
+    @Test
+    public void withParametersTest() {
+        DataTable.RowValueAccessor rowMock = Mockito.mock(DataTable.RowValueAccessor.class);
+        Mockito.when(rowMock.toStringMap()).thenReturn(Collections.singletonMap("one", "two"));
+        StartTestItemRQ event = new StartEventBuilder(ItemType.TEST)
+                .withStartTime(ZonedDateTime.now())
+                .withName("name")
+                .withParameters(rowMock)
+                .build();
+        ParameterResource expected = new ParameterResource();
+        expected.setKey("one");
+        expected.setValue("two");
+        Assert.assertEquals(Collections.singletonList(expected), event.getParameters());
+    }
+
+    @Test
+    public void withTagsTest() {
+        Set<TestTag> tags = new HashSet<>();
+        tags.add(TestTag.withName("name").andType("type"));
+        tags.add(TestTag.withName("name").andType("story"));
+        StartTestItemRQ event = new StartEventBuilder(ItemType.TEST)
+                .withStartTime(ZonedDateTime.now())
+                .withName("name")
+                .withTags(tags)
+                .build();
+        Assert.assertEquals(Collections.singleton("type:name"), event.getTags());
+    }
+}
