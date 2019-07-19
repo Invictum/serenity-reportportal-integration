b0f7d687976b80258ac5ac063a7f64f412412f39
Iaroslav Iershov <zim182@gmail.com>
2019-07-19 10:36:11 -0500

    #63: Implemented launches merge for multi-module project

--- src/main/java/com/github/invictum/reportportal/injector/SerenityPortalModule.java
+++ src/main/java/com/github/invictum/reportportal/injector/SerenityPortalModule.java
@@ -1,7 +1,10 @@
 package com.github.invictum.reportportal.injector;
 
 import com.epam.reportportal.service.Launch;
-import com.github.invictum.reportportal.*;
+import com.github.invictum.reportportal.LogStorage;
+import com.github.invictum.reportportal.LogUnitsHolder;
+import com.github.invictum.reportportal.LogUnitsHolderProvider;
+import com.github.invictum.reportportal.SuiteStorage;
 import com.google.inject.AbstractModule;
 import com.google.inject.Scopes;
 
@@ -11,7 +14,6 @@ public class SerenityPortalModule extends AbstractModule {
         bind(Launch.class).toProvider(ReportLaunchProvider.class).asEagerSingleton();
         bind(LogUnitsHolder.class).toProvider(LogUnitsHolderProvider.class).in(Scopes.SINGLETON);
         bind(LogStorage.class).in(Scopes.SINGLETON);
-        bind(ReportIntegrationConfig.class).in(Scopes.SINGLETON);
         bind(SuiteStorage.class).in(Scopes.SINGLETON);
     }
 }
